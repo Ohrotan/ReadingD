30496615a1b037051f1881bf500e722b7823551f
gmgkglgh <54494532+gmgkglgh@users.noreply.github.com>
2019-11-28 02:28:31 +0900

    슬우 레이아웃 진행중

--- app/src/main/java/com/ssu/readingd/MemoEditActivity.java
+++ app/src/main/java/com/ssu/readingd/MemoEditActivity.java
@@ -3,9 +3,7 @@ package com.ssu.readingd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-/* 작성자: 박슬우
-최초 작성일: 2019.11.16
-파일 내용: 메모를 수정할 때 화면 */
+
 public class MemoEditActivity extends AppCompatActivity {
 
     @Override
