6a08869d6a069863719d250d4f8cfb9995eee2f8
gmgkglgh <54494532+gmgkglgh@users.noreply.github.com>
2019-12-09 23:23:36 +0900

    memo delete

--- app/src/main/java/com/ssu/readingd/TestSeulwooActivity.java
+++ app/src/main/java/com/ssu/readingd/TestSeulwooActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.ssu.readingd.dto.MemoDTO;
+import com.ssu.readingd.util.DBUtil;
 
 public class TestSeulwooActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -20,6 +31,7 @@ public class TestSeulwooActivity extends AppCompatActivity implements View.OnCli
     Button btn103;
     Button btn104;
     Dialog alertDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,19 +50,9 @@ public class TestSeulwooActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         Intent intent;
         if(v == btn101){
-            AlertDialog customDialog;
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-            View customDialogView =inflater.inflate(R.layout.layout_memo_delete,null);
-            builder.setView(customDialogView);
-
-//            builder.setPositiveButton("확인", null);
-//            builder.setNegativeButton("취소", null);
-
-            customDialog = builder.create();
-            customDialog.show();
-
+            String memo_id = "iNlSlWUg7kHRyhyR7zZt";
+            CustomDialog customDialog = new CustomDialog(TestSeulwooActivity.this);
+            customDialog.callFunction(memo_id);
 
         } else if(v == btn102){
 
@@ -61,6 +63,38 @@ public class TestSeulwooActivity extends AppCompatActivity implements View.OnCli
         }else if(v == btn104) {
             intent = new Intent(this, MemoEditActivity.class);
             startActivity(intent);
+        }
+
+    }
+
+    public class CustomDialog{
+
+        private Context context;
+
+        public CustomDialog(Context context){
+            this.context= context;
+        }
+
+        public void callFunction(final String memo_id){
+            final Dialog dlg = new Dialog(context);
+
+            dlg.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dlg.setContentView(R.layout.layout_memo_delete);
+            dlg.show();
+
+            final Button deleteBtn = (Button)dlg.findViewById(R.id.memoDeleteBtn);
+            final Button cancelBtn = (Button)dlg.findViewById(R.id.memoDeleteCancelBtn);
+            deleteBtn.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+                    new DBUtil().DeleteMemo(memo_id);
+                    onBackPressed();
+                }
+            });
+            cancelBtn.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+                    onBackPressed();
+                }
+            });
         }
 
     }
