a515992cbaf5befff524dde408fb15307d63cc5e
gmgkglgh <54494532+gmgkglgh@users.noreply.github.com>
2019-12-18 03:55:01 +0900

    seul

--- app/src/main/java/com/ssu/readingd/MemoRegisterActivity.java
+++ app/src/main/java/com/ssu/readingd/MemoRegisterActivity.java
@@ -2,9 +2,12 @@ package com.ssu.readingd;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +15,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -50,6 +54,7 @@ import com.google.firestore.v1.Cursor;
 import com.ssu.readingd.util.DBUtil;
 import com.ssu.readingd.dto.MemoDTO;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
@@ -181,15 +186,7 @@ public class MemoRegisterActivity extends AppCompatActivity implements View.OnCl
                 imgIndex = imgcnt - 1;
 
                 setImageSwitcher(getApplicationContext(), imageSwitcher, imgIndex);*/
-
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
-                uploadFile();
-                imgcnt++;
-                imgIndex = imgcnt -1;
-                setImageSwitcher(getApplicationContext(), imageSwitcher, imgIndex);
+                makeDialog();
             }
         });
 
@@ -277,6 +274,69 @@ public class MemoRegisterActivity extends AppCompatActivity implements View.OnCl
         Glide.with(con).load(uri).into((ImageView) imageSwitcher.getCurrentView());
     }
 
+    private void makeDialog(){
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(MemoRegisterActivity.this);
+
+        alt_bld.setTitle("사진 업로드").setCancelable(false).setPositiveButton("사진촬영",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Log.v("알림", "다이얼로그 > 사진촬영 선택");
+                        takePhoto();
+                    }
+                }).setNegativeButton("앨범선택",
+
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int id) {
+                        Log.v("알림", "다이얼로그 > 앨범선택 선택");
+                        selectAlbum();
+                    }
+                }).setNeutralButton("취소   ",
+                new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alt_bld.create();
+        alert.show();
+    }
+
+    public void takePhoto(){
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(takePictureIntent.resolveActivity(getPackageManager())!=null){
+                File photoFile = null;
+                try{
+                    photoFile = createImageFile();
+                }catch(IOException ex){
+
+                }
+
+                if(photoFile != null){
+                    filePath = FileProvider.getUriForFile(this, getApplicationContext().getPackageName()+".fileprovider",photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, filePath);
+                    startActivityForResult(takePictureIntent, 2001);
+                    uploadFile();
+                    imgcnt++;
+                    imgIndex = imgcnt -1;
+                    setImageSwitcher(getApplicationContext(), imageSwitcher, imgIndex);
+                }
+            }
+        }
+    }
+
+    public void selectAlbum(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 2002);
+        uploadFile();
+        imgcnt++;
+        imgIndex = imgcnt -1;
+        setImageSwitcher(getApplicationContext(), imageSwitcher, imgIndex);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -305,6 +365,15 @@ public class MemoRegisterActivity extends AppCompatActivity implements View.OnCl
             Imgids2.add(filename);
         } else
             Toast.makeText(getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+    }
+
+    private File createImageFile() throws IOException{
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyyMMHH_mmss");
+        String reg_date = dateformat.format(cal.getTime());
+        String filename = reg_date + ".png";
+        File storageDir = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/pathvalue/" +filename);
+        return storageDir;
     }
 }
 
